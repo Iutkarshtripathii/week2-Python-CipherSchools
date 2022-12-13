28b5245dc14970f6166c0443920675259263617a
UTKARSH TRIPATHI <119836491+Utkarshtripath@users.noreply.github.com>
2022-12-14 01:45:17 +0530

    Create Class092-Join and Split_Method-CipherSchool.py

---
+++ Class092-Join and Split_Method-CipherSchool.py
@@ -0,0 +1,12 @@
+# split method
+user_info='harshit 24'.split()
+print(user_info)
+
+name,age='utkarsh,18'.split(",")
+print(name)
+print(age)
+
+
+#joint method
+user_info=['utkarsh','18']
+print(','.join(user_info))
