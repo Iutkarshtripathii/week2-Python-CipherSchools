fe6e1ba6bcbf2a79d88dd80c16a8bfd1cb0119da
UTKARSH TRIPATHI <119836491+Utkarshtripath@users.noreply.github.com>
2022-12-14 02:21:02 +0530

    Create Class120-Update-CipherSchool.py

---
+++ Class120-Update-CipherSchool.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'utkarsh',
+    'age' : '18',
+}
+more_info={'name' : 'Utkarsh Tripathi' , 'state' : 'Uttar Pradesh' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
