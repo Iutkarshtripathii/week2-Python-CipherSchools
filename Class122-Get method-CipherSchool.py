8a2bfaa0b5fff98181e232ba5186c49c1d6e5322
UTKARSH TRIPATHI <119836491+Utkarshtripath@users.noreply.github.com>
2022-12-14 02:25:03 +0530

    Create Class122-Get method-CipherSchool.py

---
+++ Class122-Get method-CipherSchool.py
@@ -0,0 +1,10 @@
+user={'name': 'Utkarsh' , 'age': '18'}
+print(user.get('names','not found!'))
+
+
+user={'name': 'Utkarsh' , 'age': '18', 'age': '20'}
+print(user.get('age'))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
